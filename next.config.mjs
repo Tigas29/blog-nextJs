c00669baa4c0a3e9885f44fa86ed8b19d37ef6aa
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2024-04-08 13:04:30 -0300

    commit initial

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//   }
+// };
+
+
+// export default nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
