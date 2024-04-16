a07888dfcd324575f550669273dbd89f76677c55
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2024-04-16 16:38:55 -0300

    ajusting

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
-      <h1></h1>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     serverActions: true,
-//   }
-// };
-
-
-// export default nextConfig
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
   },
+
 };
 
 
